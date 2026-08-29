93240384a152348753d47989829885a63e2291bc
M. Hamdani Ilham Latjoro <d41113512@gmail.com>
2026-08-29 21:48:13 +0800

    role vendor

--- app/Models/User.php
+++ app/Models/User.php
@@ -26,8 +26,9 @@ use Laravel\Fortify\TwoFactorAuthenticatable;
  * @property string|null $remember_token
  * @property Carbon|null $created_at
  * @property Carbon|null $updated_at
+ * @property string $role
  */
-#[Fillable(['name', 'email', 'password'])]
+#[Fillable(['name', 'email', 'password', 'role'])]
 #[Hidden(['password', 'two_factor_secret', 'two_factor_recovery_codes', 'remember_token'])]
 class User extends Authenticatable implements PasskeyUser
 {
